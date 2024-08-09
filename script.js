e6e2bd2c3fada1ff23e39a66e9e4d146a412b707
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-09 17:24:25 +0500

    Первые 2 числа

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ for(let i = 0; i<array.length; i++) {
 
 console.log("Сумма цифр числа равна" + " " + sum);
 console.log("Умножение:" + " " + umno);
+
+let stepSum;
+stepSum = sum ** 3;
+console.log(stepSum);
+
+let stepUmno;
+stepUmno = umno ** 3;
+console.log(stepUmno);
+
+let array1 = [];
+array1 = Array.from(String(stepSum), Number); 
+console.log(array1[0], array1[1]);
+
+let array2 = [];
+array2 = Array.from(String(stepUmno), Number); 
+console.log(array2[0], array2[1]);
+
