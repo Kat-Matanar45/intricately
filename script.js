9ff09ed37c4008939c584ee7dc7b271b4048eaa1
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-13 16:09:29 +0500

    Не работает код

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
+'use strict';
+/** document = "index.html";
+ 
+const exampleAttr = document.querySelector("html");
+const lang = exampleAttr.getAttribute("lang");
+console.log(exampleAttr, lang); */ 
+// увы, но так и не заработал этот код
 
+let lang = 'ru';
 
+if (lang = 'ru') {
+    console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+} else (lang = 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+switch(true) {
+    case lang = "ru":
+        console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+        break
+    case lang = "en":
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break
+}
+
+let namePerson = "Артем";
+namePerson = "Артем" ? console.log('Директор') : (= namePerson = "Александр" ? console.log('Преподаватель') : (= namePerson = !"Артем" && !"Александр" ? console.log('Студент')))
+    
