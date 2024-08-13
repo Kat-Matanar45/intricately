6a0fd6791dd69eed81941e9402bf27adbca0ee37
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-13 16:41:32 +0500

    Остался только массив

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ const lang = exampleAttr.getAttribute("lang");
 console.log(exampleAttr, lang); */ 
 // увы, но так и не заработал этот код
 
-let lang = 'ru';
+let lang = "ru";
 
-if (lang = 'ru') {
+if (lang == "ru") {
     console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-} else (lang = 'en') {
+} else if (lang == "en") {
     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 }
 
 switch(true) {
-    case lang = "ru":
+    case lang == "ru":
         console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
         break
-    case lang = "en":
+    case lang == "en":
         console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
         break
 }
 
-let namePerson = "Артем";
-namePerson = "Артем" ? console.log('Директор') : (= namePerson = "Александр" ? console.log('Преподаватель') : (= namePerson = !"Артем" && !"Александр" ? console.log('Студент')))
-    
+
+
+let namePerson = "Александр";
+namePerson == "Артем" ? console.log('Директор') : namePerson == "Александр" ? console.log('Преподаватель') : (namePerson != "Артем") && (namePerson != "Александр") ? console.log('Студент') : stop
