7693de668226401574874500a4b66f28d2a9fd61
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-17 13:49:45 +0500

    все работает

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let argument = prompt("Какая у вас сегодня погода? И как вы планируете провести сегодняшний день?");
+let type;
+
+if (!isNaN(argument)) {
+    argument = parseFloat(argument);
+    type = typeof argument;
+} else {
+    type = typeof argument;
+}
+
+function oneArgument (type, weather) {
+    if (type !== "string") {return "Принимаются только буквенные значения!"}
+    weather = weather.trim();
+    if (weather.length > 30) {return weather.substr(0, 30) + '...';} else {return weather}
+}
+
+console.log(type);
+console.log(oneArgument(type, argument));
