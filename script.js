aada645eaafe190edb06665d773e0ead5931c824
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-20 18:54:05 +0500

    Выполнено усложненное 1

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+let array = [20,13,14,8,45,3,11];
+
+for (let i=0; i<array.length; i++) {
+    let currentElem = array[i];
+    currentElem = String(currentElem);
+    if ((currentElem.charAt(0) === '2') || (currentElem.charAt(0) === '4')) {
+        console.log(currentElem);
+    }};
