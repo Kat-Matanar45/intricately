36201d204a1d5bfe1f2a813c28719d1475acf7b9
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-20 19:01:27 +0500

    Сложное задание выполнено

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ for (let i=0; i<array.length; i++) {
     if ((currentElem.charAt(0) === '2') || (currentElem.charAt(0) === '4')) {
         console.log(currentElem);
     }};
+
+
+for (let n=0; n<=100; n++) {
+    console.log(n, 'Делители этого числа: 1 и ' + n);
+}
+
+let n=0;
+while (n>=100) {
+    console.log(n, 'Делители этого числа: 1 и ' + n);
+}
