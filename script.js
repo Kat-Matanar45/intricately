7d2680979cbac4f5249aab105dd8cc6cc9f99151
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-23 16:17:06 +0500

    Ошибки исправлены и все работает

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ let array = [20,13,14,8,45,3,11];
 for (let i=0; i<array.length; i++) {
     let currentElem = array[i];
     currentElem = String(currentElem);
-    if ((currentElem.charAt(0) === '2') || (currentElem.charAt(0) === '4')) {
+    if ((currentElem.startsWith("2") === true) || (currentElem.startsWith("4") === true)) {
         console.log(currentElem);
     }};
 
-
-for (let n=0; n<=100; n++) {
-    console.log(n, 'Делители этого числа: 1 и ' + n);
+for (let n=1; n<=100; n++) {
+    let array = [];
+for (let h=1; h<=n; h++) {
+    let division = n%h;
+    if (division === 0) {array.push(h);}
 }
-
-let n=0;
-while (n>=100) {
+if (array.length == 2) {   
     console.log(n, 'Делители этого числа: 1 и ' + n);
-}
+}}
+
