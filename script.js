28a3986c56916ca05a46c56efe500b461c1fe160
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-29 17:01:36 +0500

    Не работает жирный и курсив

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+console.log(week);
+
+for (let i=0; i<week.length; i++) {
+    console.log(week[i]);
+
+    if ((week[i] === "Суббота")) {
+        let str = week[i];
+        week[i] = str.bold();
+    }
+}
+
