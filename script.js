41de857582d46fd66a2952b3d820c09e466247c7
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-03 16:38:22 +0500

    Файл очищен

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 'use strict';
 
-let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-
-console.log(week);
-
-for (let i=0; i<week.length; i++) {
-    console.log(week[i]);
-
-    if ((week[i] === "Суббота")) {
-        let str = week[i];
-        week[i] = str.bold();
-    }
-}
 
