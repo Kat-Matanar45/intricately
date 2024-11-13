b020ec66abdc2dea6ef839fdc604b533a3325981
Kat Tyushnyakova <avchimchik@gmail.com>
2024-11-13 17:25:48 +0500

    Работает

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 'use strict';
 
 let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+const divWeek = document.createElement('div');
+const date = new Date();
+const nDay = date.getDay();
 
-console.log(week);
+document.body.append(divWeek);
+divWeek.textContent = week;
 
 for (let i=0; i<week.length; i++) {
-    console.log(week[i]);
+    const pWeek = document.createElement('p');
+    divWeek.append(pWeek);
+    pWeek.textContent = week[i];
 
-    if ((week[i] === "Суббота")) {
-        let str = week[i];
-        week[i] = str.bold();
+    if ((week[i] === 'Суббота') || (week[i] === 'Воскресенье')) {
+        pWeek.style.fontStyle = 'italic';
+    };
+
+    if ((i+1) === nDay) {
+        pWeek.style.fontWeight = '700';
     }
+
 }
 
+console.log(week);
